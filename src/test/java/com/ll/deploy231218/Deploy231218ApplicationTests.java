31093610da8f7841b59022c86b7b1c0907a73243
jae9380 <ljy5314@gmail.com>
2023-12-21 21:36:30 +0900

    work

---
+++ src/test/java/com/ll/deploy231218/Deploy231218ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.deploy231218;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy231218ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
